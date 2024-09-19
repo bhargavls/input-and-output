0cef7fc0e142ad18d94af2b46a6a125689663973
bhargavls <22hr1a3228@mtieat.org>
2024-09-19 14:13:10 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,146 @@
+''''
+List
+Write a program to create a list and display it. 
+Input format:
+Input consist of n+1 integers
+First integer corresponds to the size of the list
+Next n inputs corresponds to the elements in the list 
+Output format: 
+Output is an integer list
+Sample Input
+4 
+1
+2
+3
+4
+Output
+[1, 2, 3, 4]
+
+
+2)Write a program to create a list and display it. 
+Input format:
+Input consist of n+1 integers
+First integer corresponds to the size of the list
+Next n inputs corresponds to the elements in the list 
+Output format: 
+Output is an integer list
+Sample Input
+4 
+1
+2
+3
+4
+Output
+1 2 3 4
+3)Write a Python Program to find the largest number in a list
+Input & Output Format:
+Input consists of one list and one integer.
+First input consists of a size of a list.
+Second inputs corresponds to the size of the list.
+Output consists of the largest element.
+Sample Input:
+5
+1
+2
+3
+6
+5
+Sample Output:
+6
+4)Write a Python Program to put the even and odd elements in a list into two different lists.
+Input format:
+Input consists of one integer and one list.
+First input consists of the size of the list.
+Second input consists of the elements based on the size.
+Output format:
+Output consists of two lists.
+First list consists of all the even numbers in the list.
+Second list consists of all the odd numbers in the list.
+Sample Input:
+5
+1
+2
+3
+6
+5
+Sample Output:
+The even list [2, 6]
+The odd list [1, 3, 5]
+5)Write a program to find the sum of the elements in the array. 
+Input Format: 
+Input consists of n+1 integers where n corresponds to the number of elements in the array.
+The first integer corresponds to n and the next n integers correspond to the elements in the array.
+Assume that the maximum number of elements in the array is 20. 
+Output Format: 
+Output consists of a double value which corresponds to the mean of the array.
+Refer sample input and output for formatting specifications.
+Sample Input: 
+5
+2
+4
+1
+3
+1
+Sample Output:
+11
+6)Write a Python Program to find the smallest number in a list
+Input & Output Format:
+Input consists of one list and one integer.
+First input consists of a size of a list.
+Second inputs corresponds to the size of the list.
+Output consists of the largest element.
+Sample Input:
+5
+1
+2
+3
+6
+5
+Sample Output:
+1
+
+
+7)Write a Program to find the search element in the given list
+Input & Output Format:
+Input consists of one list and one integer.
+First input consists of a size of a list.
+Second inputs corresponds to the elements of list in single line separated by space.
+Third input consists of the element which we need to search
+Sample Input 1:
+5
+1 2 3 6 5
+3
+Sample Output 1:
+3 is present in the given list
+Sample Input 2:
+5
+1 2 3 6 5
+4
+Sample Output 2:
+4 is not present in the given list
+8)Write a program to count the number of times the given value is repeated.
+Input Format:
+First line of input consists of our list elements.
+Second line of input consists of value to count.
+Output Format:
+Print thr number of times the given value is repeated in the list.
+Sample Input:
+10 20 10 40 10
+10
+Sample Output:
+3
+9)Write a program to print the given list in reverse order.
+Sample Input:
+10 20 30 40 50
+Sample Output:
+50 40 30 20 10 
+10)Write a program to sort the given list and print it.
+Sample Input:
+30 20 10 50 40
+Sample Output:
+10 20 30 40 50 
+
+
+
+
+
